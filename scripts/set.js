dd9217cf7c08a44a1bd36a380b258ba7951ff9bb
Kuenkemguilys <kuenkemguilys@gmail.com>
2024-06-04 17:50:46 +0100

    Update set.js
    Signed-off-by: Kuenkemguilys <kuenkemguilys@gmail.com>

--- scripts/set.js
+++ scripts/set.js
@@ -18,9 +18,9 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["100090405019929"];
+    const permission = ["61553436962117"];
   if (!permission.includes(event.senderID)) {
-    api.sendMessage("𝙄𝙙𝙞𝙤𝙩...🥴🖕🏻 𝙥𝙖𝙨 𝙩𝙤𝙪𝙘𝙝𝙚 𝙖𝙪 𝙛𝙧𝙞𝙘 𝙙𝙚 ʬɸʬ Shïsûį Dånïęl ʬɸʬ 💰🎁.", event.threadID, event.messageID);
+    api.sendMessage("𝙄𝙙𝙞𝙤𝙩...🥴🖕🏻 𝙥𝙖𝙨 𝙩𝙤𝙪𝙘𝙝𝙚 𝙖𝙪 𝙛𝙧𝙞𝙘 𝙙𝙚 ロɞ𝗭𝗲𝘂𝘀ɞ 𝗭𝗲𝗻𝗼 く瀬 💰🎁.", event.threadID, event.messageID);
     return;
   }
     const query = args[0];
